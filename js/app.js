922abace473c140831b862a6ef6841d81a84ffaa
unknown <forinstagram9887@gmail.com>
2024-01-28 17:38:55 +0500

    js

--- js/app.js
+++ js/app.js
@@ -1,15 +1,54 @@
-function appendToResult(value) {
-    document.getElementById('result').value += value;
-  }
+document.addEventListener('DOMContentLoaded', function () {
+    const display = document.getElementById('display');
+    const buttons = document.getElementById('buttons');
 
-  function clearResult() {
-    document.getElementById('result').value = '';
-  }
+    let currentInput = '';
+    let operator = '';
+    let previousInput = '';
 
-  function calculateResult() {
-    try {
-      document.getElementById('result').value = eval(document.getElementById('result').value);
-    } catch (error) {
-      document.getElementById('result').value = 'Ошибка';
+    buttons.addEventListener('click', function (event) {
+      const clickedButton = event.target.innerText;
+
+      if (!isNaN(clickedButton) || clickedButton === '.') {
+        currentInput += clickedButton;
+      } else if (clickedButton === 'C') {
+        currentInput = '';
+        operator = '';
+        previousInput = '';
+      } else if (clickedButton === '=') {
+        currentInput = evaluate();
+        operator = '';
+        previousInput = '';
+      } else {
+        if (operator && previousInput && currentInput) {
+          currentInput = evaluate();
+          previousInput = currentInput;
+        } else {
+          previousInput = currentInput;
+        }
+
+        operator = clickedButton;
+        currentInput = '';
+      }
+
+      display.innerText = currentInput || '0';
+    });
+
+    function evaluate() {
+      const num1 = parseFloat(previousInput);
+      const num2 = parseFloat(currentInput);
+
+      switch (operator) {
+        case '+':
+          return (num1 + num2).toString();
+        case '-':
+          return (num1 - num2).toString();
+        case '*':
+          return (num1 * num2).toString();
+        case '/':
+          return (num1 / num2).toString();
+        default:
+          return currentInput;
+      }
     }
-  }
+  });
